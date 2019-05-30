45d29c40efe81fdd75fb8ef0f57949dc651de277
jesse <jclifton333@gmail.com>
2019-05-30 14:35:26 +0200

    implement model fitting for each dataset

--- analysis.py
+++ analysis.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
+from sklearn.metrics import balanced_accuracy_score
 
 
 if __name__ == "__main__":
   data = pd.read_csv("newcomb-data.csv")
+  exluded_study_labels = (np.nan, 20)
 
   # Remove columns not needed for analysis
   cols_to_remove = ["StartDate", "EndDate", "Platform", "IPAddress", "workerId", "Duration__in_seconds_",
@@ -15,7 +19,6 @@ if __name__ == "__main__":
 
   # Create separate dataframes for each study
   dataframes_for_each_study = {}
-  exluded_study_labels = (np.nan, 20)
   for study_number in data.Study.unique() if not study_number in exluded_study_labels:
     data_for_study = data[data["Study"] == study_number]
 
@@ -30,5 +33,41 @@ if __name__ == "__main__":
     dataframes_for_each_study[study_number] = (X_for_study, y_for_study)
 
   # For each study, collect data from other studies with same features, train predictive model, and evaluate
+  results = {}
+  for test_study_number, X_and_y_test in dataframes_for_each_study.items():
+    X_test, y_test = X_and_y_test
+    feature_names = X_test.columns
+    X_train = pd.DataFrame(columns=feature_names)
+    y_train = np.zeros(0)
+    overlapping_studies = []
+
+    # Build training data
+    for study_number, X_and_y_ in dataframes_for_each_study if study_number != test_study_number:
+      # Check if train study contains same features as test study; if so, add to dataset
+      X_, y_ = X_and_y_
+      if np.array_equal(np.intersect1d(X_.columns, feature_names), feature_names):
+        X_train = pd.concat([X_train, X_[feature_names]])
+        y_train = np.append(y_train, y_)
+        overlapping_studies.append(study_number)
+
+    if overlapping_studies is not None:
+      # Fit model
+      clf = RandomForestClassifier()
+      selector = RFE(clf)
+      selector.fit(X_train, y_train)
+
+      # Train and test accuracy
+      train_predictions = selector.estimator_.predict(X_train)
+      train_acc = balanced_accuracy_score(y_train, train_predictions)
+      test_predictions = selector.estimator_.predict(X_test)
+      test_acc = balanced_accuracy_score(y_test, test_predictions)
+
+      # Add to results dict
+      study_results = {'train_acc': train_acc, 'test_acc': test_acc, 'selected_features':
+                               [n for i, n in enumerate(feature_names) if selector.support_[i]]}
+      results[test)study_number] = study_results
+      print('study no {}\n{}'.format(test_study_number, study_results))
+
+
 
 
