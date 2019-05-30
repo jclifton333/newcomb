0f6bc575d35270990d4bbc1f124a2289dd6fd3f6
jesse <jclifton333@gmail.com>
2019-05-30 22:07:26 +0200

    numerify!

--- analysis.py
+++ analysis.py
@@ -1,19 +1,18 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import RFE
+from sklearn.feature_selection import RFE, RFECV
 from sklearn.metrics import recall_score
 import pdb
 import random
 
 RANDOM_SEED = 3
-RESULTS_FNAME = "newcomb-oos-results-2.csv"
-SAVE = False
+RESULTS_FNAME = "results/newcomb-oos--rfecv-2-with-payoffs.csv"
+SAVE = True
 EXCLUDED_STUDY_LABELS = (np.nan, 20)
 PAYOFF_DICT = {1.0: (3, 0.05), 2.0: (2.55, 0.45), 3.0: (2.25, 0.75), 12.0: (3, 0.50), 13.0: (2.5, 0.25),
                14.0: (2.5, 0.25), 15.0: (4, 0.5), 16.0: (4, 0.5), 17.0: (4, 0.5), 18.0: (4, 0.5), 19.0: (2.23, 0.28),
                20.0: (2.23, 0.11), 21.0: (2.23, 0.11), 22.0: (2, 0.1)}  # Payoffs for (box 1, box 2) in each study
-
 
 
 def balanced_accuracy_score(y_true, y_pred):
@@ -52,7 +51,7 @@ if __name__ == "__main__":
       data_for_study.replace(' ', np.nan, regex=True, inplace=True)
       data_for_study.dropna(axis=1, how='all', inplace=True)
       data_for_study.dropna(axis=0, how='any', inplace=True)
-      data_for_study.applymap(float)
+      data_for_study = data_for_study.applymap(float)
       data_for_study = data_for_study[data_for_study["newcomb_combined"] != 0.0]
       print("Study {} data size {}".format(study_number, data_for_study.shape))
 
@@ -61,7 +60,6 @@ if __name__ == "__main__":
         X_for_study['payoff1'] = PAYOFF_DICT[study_number][0]
         X_for_study['payoff2'] = PAYOFF_DICT[study_number][1]
         y_for_study = data_for_study.newcomb_combined
-
         dataframes_for_each_study[study_number] = (X_for_study, y_for_study)
 
   # For each study, collect data from other studies with same features, train predictive model, and evaluate
@@ -89,7 +87,7 @@ if __name__ == "__main__":
 
       # Fit model
       clf = RandomForestClassifier(oob_score=True, n_estimators=100)
-      selector = RFE(clf)
+      selector = RFECV(clf)
       selector.fit(X_train, y_train)
 
       # Train and test accuracy
